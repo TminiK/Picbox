21bff498f13251b35efeb6b3888230b0ce033113
Mark <luo9757@gmail.com>
2021-10-20 14:43:46 +0800

    1

--- app/src/main/java/ml/timik/picbox/configs/Names.java
+++ app/src/main/java/ml/timik/picbox/configs/Names.java
@@ -5,7 +5,7 @@ package ml.timik.picbox.configs;
  */
 
 public class Names {
-    public static final String appdirname = "图箱";
+    public static final String appdirname = "Picbox";
     public static final String backupdirname = "Backup";
     public static final String sitename = "Sites.json";
     public static final String settingname = "Setting.json";
